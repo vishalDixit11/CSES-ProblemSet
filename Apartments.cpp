b861e798d3fe23b4fd5c978d76aa55f964c5fdd3
vishalDixit11 <vishaldixit4493@gmail.com>
2024-10-17 12:47:08 +0530

    Create Apartments.cpp
    CSES problem set (sorting n searching ) 2nd question

---
+++ Apartments.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m,k;
+    cin>>n>>m>>k;
+
+    //Taking inputs
+    vector<int> desired(n);
+    vector<int> actual(m);
+
+    for(int i=0;i<n;i++){
+        cin>>desired[i];
+    }
+
+    for(int j=0;j<m;j++){
+        cin>>actual[j];
+    }
+
+
+    //Actual algo
+
+}
